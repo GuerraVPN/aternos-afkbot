f67ef1bf293f70af248ea6d7d876378695fa84c4
GuerraVPN <81371258+GuerraVPN@users.noreply.github.com>
2021-12-14 15:39:37 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mineflayer = require('mineflayer')
+const cmd = require('mineflayer-cmd').plugin
 const fs = require('fs');
 let rawdata = fs.readFileSync('config.json');
 let data = JSON.parse(rawdata);
@@ -11,21 +12,31 @@ var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
 var host = data["ip"];
-var port = data["port"];
 var username = data["name"]
+var nightskip = data["auto-night-skip"]
 var bot = mineflayer.createBot({
   host: host,
-  port: port,
   username: username
 });
 function getRandomArbitrary(min, max) {
        return Math.random() * (max - min) + min;
 
 }
+
+bot.loadPlugin(cmd)
+
+
+
 bot.on('login',function(){
 	console.log("Logged In")
+	bot.chat("hello");
 });
-bot.on('time', function() {
+
+bot.on('time', function(time) {
+	if(nightskip == "true"){
+	if(bot.time.timeOfDay >= 13000){
+	bot.chat('/time set day')
+	}}
     if (connected <1) {
         return;
     }
@@ -55,5 +66,9 @@ bot.on('time', function() {
 
 bot.on('spawn',function() {
     connected=1;
+});
+
+bot.on('death',function() {
+    bot.emit("respawn")
 });
 
